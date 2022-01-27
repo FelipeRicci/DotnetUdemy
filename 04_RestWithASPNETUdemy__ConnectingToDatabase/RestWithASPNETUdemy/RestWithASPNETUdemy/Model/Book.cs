9e9cdcddc69d180ebacfc4e009c00a14d8c463ac
FelipeRicci <felipergotarde375@gmail.com>
2022-01-27 16:00:37 -0300

    Connection To DataBase

---
+++ 04_RestWithASPNETUdemy__ConnectingToDatabase/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETUdemy.Model
+{
+    [Table("books")]
+    public class Book
+    {
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("livro")]
+        public string Livro { get; set; }
+
+        [Column("autor")]
+        public string Autor { get; set; }
+
+        [Column("pagina")]
+        public string Pagina { get; set; }
+    }
+}
