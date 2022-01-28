7e1793fbb0095a44fdea7ad1e103d19c7cdaf491
FelipeRicci <felipergotarde375@gmail.com>
2022-01-28 15:59:56 -0300

    Working With Value Objects

---
+++ 09_RestWithASPNETUdemy_WorkingWithValueObjects/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -0,0 +1,13 @@
+﻿namespace RestWithASPNETUdemy.Data.VO
+{
+    public class BookVO
+    {
+        public long Id { get; set; }
+
+        public string Livro { get; set; }
+
+        public string Autor { get; set; }
+
+        public string Pagina { get; set; }
+    }
+}
---
+++ 09_RestWithASPNETUdemy_WorkingWithValueObjects/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+  (1, 'Ayrton', 'Senna', 'Sao Paulo - Brasil', 'Male'),
+  (2, 'Leonardo', 'da Vinci', 'Anchiano - Italy', 'Male'),
+  (3, 'Mahatma', 'Ganghi', 'Porbandar - India', 'Male'),
+  (4, 'Mohamed Ali', 'Ganghi', 'Kentucky - USA', 'Male'),
+  (5, 'Nelson', 'Mandela', 'Mvezo - South Africa', 'Male'); 
