7b5b9ff9a3ca6251f79f53e862fc9d35e590416a
FelipeRicci <felipergotarde375@gmail.com>
2022-01-28 16:06:11 -0300

    Custom Serialization

---
+++ 10_RestWithASPNETUdemy_CustomSerialization/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V4__Populate_Table_Books.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO `books` (`id`, `livro`, `autor`, `pagina`) VALUES
+  (1, 'Ayrton', 'Senna', 200),
+  (2, 'Leonardo', 'da Vinci', 150),
+  (3, 'Mahatma', 'Ganghi', 400),
+  (4, 'Mohamed Ali', 'Ganghi', 22);
