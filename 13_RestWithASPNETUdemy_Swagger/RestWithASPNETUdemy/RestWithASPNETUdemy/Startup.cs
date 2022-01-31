6ab3236658f2054a9e721545ddf6aa655e5c9531
FelipeRicci <felipergotarde375@gmail.com>
2022-01-31 15:58:57 -0300

    Adding Swagger Support

---
+++ 13_RestWithASPNETUdemy_Swagger/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -0,0 +1,144 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Business.Implementations;
+using RestWithASPNETUdemy.Hypermedia.Enricher;
+using RestWithASPNETUdemy.Hypermedia.Filters;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository;
+using RestWithASPNETUdemy.Repository.Generic;
+using Serilog;
+using System;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy
+{
+    public class Startup
+    {
+
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+           
+            services.AddControllers();
+
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            if (Environment.IsDevelopment())
+            {
+                MigrateDataBase(connection);
+            }
+
+            var filterOptions = new HyperMediaFilterOptions();
+            filterOptions.ContentResponseEnricherList.Add(new PersonEnricher());
+            filterOptions.ContentResponseEnricherList.Add(new BookEnricher());
+
+            services.AddSingleton(filterOptions);
+
+            //Versionamento de API
+            services.AddApiVersioning();
+
+            //Adicionando Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "REST API's From 0 to Azure with ASP .NET Core 5 and Docker",
+                        Version = "v1",
+                        Description = "API Restful developed in course 'REST API's From 0 to Azure with ASP .NET Core 5 and Docker'",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Felipe Ricci",
+                            Url = new Uri("https://github.com/FelipeRicci")
+                        }
+                    });
+            });
+
+            //Injeção de Dependencias Person
+            services.AddScoped<IPersonBusiness, PersonBusinessImplementations>();
+
+            //Injeção de Dependencias Book
+            services.AddScoped<IBookBusiness, BookBusinessImplementations>();
+
+            //Injeção de Dependencia Pet
+            services.AddScoped<IPetBusiness, PetBusinessImplementations>();
+
+            //Injeção de Dependencia Generica
+            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "REST API's From 0 to Azure with ASP .NET Core 5 and Docker - v1");
+            });
+
+
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
+            app.UseRewriter(option);
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapControllerRoute("DefaultApi", "{controller=values}/{id?}");
+            });
+        }
+
+        private void MigrateDataBase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+                evolve.Migrate();
+            }
+            catch (System.Exception ex)
+            {
+                Log.Error("DataBase migration failed", ex);
+                throw;
+            }
+        }
+    }
+}
