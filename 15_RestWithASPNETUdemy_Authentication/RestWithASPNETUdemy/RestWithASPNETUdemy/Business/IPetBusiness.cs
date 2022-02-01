61af45702cdc2c7a3dee4de88342f09777c4b237
FelipeRicci <felipergotarde375@gmail.com>
2022-02-01 16:28:26 -0300

    Adding Authentication Support

---
+++ 15_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IPetBusiness.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IPetBusiness
+    {
+        Pet Create(Pet pet);
+
+        Pet Update (Pet pet);
+
+        Pet FindById(long id);
+
+        List<Pet> FindAll();
+
+        void Delete(long id);
+    }
+}
---
+++ 15_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Pet.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNETUdemy.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETUdemy.Model
+{
+    [Table("pet")]
+    public class Pet : BaseEntity
+    {
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Column("raca")]
+        public string Raca { get; set; }
+
+        [Column("idade")]
+        public string Idade { get; set; }
+    }
+}
