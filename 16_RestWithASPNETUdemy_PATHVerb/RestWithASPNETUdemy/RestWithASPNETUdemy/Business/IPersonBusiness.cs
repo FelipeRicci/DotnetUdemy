a413fcc31a544d4b8d19a6165b73c1184df3b0b9
FelipeRicci <felipergotarde375@gmail.com>
2022-02-02 10:28:18 -0300

    Working with PATCH Verb

---
+++ 16_RestWithASPNETUdemy_PATHVerb/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IPersonBusiness.cs
@@ -0,0 +1,21 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IPersonBusiness {
+
+        PersonVO Create(PersonVO person);
+
+        PersonVO FindById(long id);
+
+        PersonVO Update(PersonVO person);
+
+        List<PersonVO> FindAll();
+
+        PersonVO Disable(long id);
+
+        void Delete(long id);
+
+    }
+}
---
+++ 16_RestWithASPNETUdemy_PATHVerb/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNETUdemy.Hypermedia;
+using RestWithASPNETUdemy.Hypermedia.Abstract;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class BookVO : ISupportsHyperMedia
+    {
+        public long Id { get; set; }
+
+        public string Livro { get; set; }
+
+        public string Autor { get; set; }
+
+        public string Pagina { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+    }
+}
